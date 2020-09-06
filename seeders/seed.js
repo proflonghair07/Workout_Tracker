299753eb13e66c508d37e2c75cfa1d14304c2e27
omniclause <vaughnmurphy89@gmail.com>
2020-09-06 18:28:01 -0500

    deploying

--- seeders/seed.js
+++ seeders/seed.js
@@ -7,7 +7,7 @@ let db = require("../models");
 // });
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
     useNewUrlParser: true,
   })
   .then(() => console.log("Connected to MongoDB successfully!"))
