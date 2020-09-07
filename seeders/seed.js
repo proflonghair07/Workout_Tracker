7226fa4ce94790c32350bf2fd07f32cc9bd38ff4
omniclause <vaughnmurphy89@gmail.com>
2020-09-06 20:23:52 -0500

    fixing heroku db

--- seeders/seed.js
+++ seeders/seed.js
@@ -7,7 +7,7 @@ let db = require("../models");
 // });
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/fitness-tracker", {
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
   })
   .then(() => console.log("Connected to MongoDB successfully!"))
