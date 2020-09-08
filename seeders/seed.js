93bd60050f37f5cc09dcbf4ac99bcd9d9118d21a
omniclause <vaughnmurphy89@gmail.com>
2020-09-08 16:07:01 -0500

    fixed connection?

--- seeders/seed.js
+++ seeders/seed.js
@@ -7,7 +7,7 @@ let db = require("../models");
 // });
 
 mongoose
-  .connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+  .connect(process.env.MONGOD_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
   })
   .then(() => console.log("Connected to MongoDB successfully!"))
